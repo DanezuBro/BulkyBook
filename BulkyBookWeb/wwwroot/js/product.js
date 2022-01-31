11713cd3026fbc5340fa097cef9ca05fc9f9303a
scortarudaniel <scortarudaniel@yahoo.com>
2022-01-31 20:25:49 +0200

    Add Home page cards, add Details Page for books, etc

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -14,19 +14,20 @@ function loadDataTable() {
             { "data": "isbn", "width": "15%" },
             { "data": "price", "width": "15%" },
             { "data": "author", "width": "15%" },
-            { "data": "category.name", "width": "10%" },
-            { "data": "coverType.name", "width": "10%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "coverType.name", "width": "15%" },
             {
                 "data": "id",
+                "width": "10%",
+                "className": "dt-body-center",
                 "render": function (data) {
                     return `
                         <div class="btn-group" role="group">
-                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2"><i class="bi bi-pencil-square"></i> Edit</a>
-                           <a onClick="Delete('/Admin/Product/Delete/${data}')" class="btn btn-danger mx-2 ms-2"><i class="bi bi-trash"></i> Delete</a>
+                            <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-2 btn-sm"><i class="bi bi-pencil-square"></i></a>
+                           <a onClick="Delete('/Admin/Product/Delete/${data}')" class="btn btn-danger mx-2 ms-2 btn-sm"><i class="bi bi-trash"></i></a>
                         </div>
                                 `
-                },
-                "width": "20%"
+                }
             }
 
         ]
