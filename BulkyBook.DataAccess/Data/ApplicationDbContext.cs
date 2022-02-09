7725b0c6b823edbec7f4332c109ad13baaa45eb5
scortarudaniel <scortarudaniel@yahoo.com>
2022-02-09 09:42:14 +0200

    Add OrderHeader and OrderDetail to ApplicationDbContext and add-migration. Add Repositoy and IRepository for OrderHeader and OrderDetail. Add properties to UnitOfWork (class and interface)

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -17,5 +17,8 @@ namespace BulkyBook.DataAccess
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+        public DbSet<OrderHeader> OrderHeaders { get; set; }
+        public DbSet<OrderDetail> OrderDetail { get; set; }
     }
 }
