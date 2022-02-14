04fa277b5192bdef10e4e99dfe4c342977f2683e
scortarudaniel <scortarudaniel@yahoo.com>
2022-02-14 09:52:18 +0200

    Add Email Sender

--- BulkyBook.Utility/EmailSender.cs
+++ BulkyBook.Utility/EmailSender.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using MailKit.Net.Smtp;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +13,21 @@ namespace BulkyBook.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("contact@bookstore.com")); // adresa de la from  asa va arata
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html){ Text=htmlMessage};// aici in htmlMessage poti defini un template html
+
+            // send email
+            using (var emailClient = new SmtpClient())
+			{
+                emailClient.Connect("smtp.gmail.com",587,MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("scortarudaniel1@gmail.com","Admin.1234");
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+			}
+
             return Task.CompletedTask;
         }
     }
